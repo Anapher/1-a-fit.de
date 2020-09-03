847cc02a74492660d14ccef7d1f7e106acc4dbe3
Vincent (Anapher) <anapher@protonmail.com>
2020-09-03 19:00:00 +0200

    add fitness offer cards

--- src/pages/index-sections/FitnessInfo.tsx
+++ src/pages/index-sections/FitnessInfo.tsx
@@ -14,6 +14,66 @@ import Img from "gatsby-image";
 import React, { useRef, useState } from "react";
 import { container } from "../../style/shared";
 
+type FitnessOffer = {
+  desc: string;
+  title: React.ReactNode;
+  imgFluid: any;
+};
+
+type ImageInfo = {
+  name: string;
+  childImageSharp: { fluid: any };
+};
+
+const getFitnessOffers: (images: ImageInfo[], data: any) => FitnessOffer[] = (
+  images,
+  { egymLogo }
+) => [
+  {
+    desc: "Moderne Fitnessgeräte von Matrix",
+    title: "Moderne Fitnessgeräte von Matrix",
+    imgFluid: images.find(x => x.name === "matrix").childImageSharp.fluid,
+  },
+  {
+    desc: "Precor",
+    title: "Intuitive Ausdauergeräte von Precor",
+    imgFluid: images.find(x => x.name === "precor").childImageSharp.fluid,
+  },
+  {
+    desc: "Moderne egym Fitness Geräte",
+    title: (
+      <>
+        Entdecke die nächste Generation Fitnessgeräte von
+        <Img
+          fixed={egymLogo.childImageSharp.fixed}
+          style={{ marginLeft: 12 }}
+        />
+      </>
+    ),
+    imgFluid: images.find(x => x.name === "egym").childImageSharp.fluid,
+  },
+  {
+    desc: "Galileo",
+    title: "Hier gibt's Galileo Geräte",
+    imgFluid: images.find(x => x.name === "galileo").childImageSharp.fluid,
+  },
+  {
+    desc: "Outdoor",
+    title: "Outdoor Training",
+    imgFluid: images.find(x => x.name === "outdoor").childImageSharp.fluid,
+  },
+  {
+    desc: "Trugge",
+    title: "Getränkeflatrate",
+    imgFluid: images.find(x => x.name === "coffee").childImageSharp.fluid,
+  },
+  {
+    desc: "Kaffee",
+    title: "Kaffee",
+    imgFluid: images.find(x => x.name === "coffee").childImageSharp.fluid,
+  },
+];
+
 const useStyles = makeStyles(theme => ({
   container: {
     ...container,
@@ -41,18 +101,14 @@ const useStyles = makeStyles(theme => ({
 
 export default function FitnessInfo() {
   const classes = useStyles();
-  const downloadElement = useRef();
-  const [signUpDialogOpen, setSignUpDialogOpen] = useState(false);
 
   const {
     fitness,
     lifestyle,
     health,
-    egym,
-    egymApp,
-    globus,
+    offers: { edges },
+    egymLogo,
     contract,
-    allFile: { edges },
   } = useStaticQuery(graphql`
     query {
       fitness: file(relativePath: { eq: "overview/fitness.jpg" }) {
@@ -76,32 +132,26 @@ export default function FitnessInfo() {
           }
         }
       }
-      egym: file(relativePath: { eq: "landing-page/egym/logo.png" }) {
+      egymLogo: file(
+        relativePath: { eq: "landing-page/offers/egym_logo.png" }
+      ) {
         childImageSharp {
           fixed(width: 80) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      egymApp: file(
-        relativePath: {
-          eq: "landing-page/egym/eGym_Fitness_App_Trainingsplan_direkt_aufs_Handy.png"
-        }
+      offers: allFile(
+        filter: { relativePath: { regex: "/landing-page/offers/" } }
       ) {
-        childImageSharp {
-          fluid(maxWidth: 600) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      globus: file(
-        relativePath: {
-          eq: "landing-page/egym/PT_w30m30trainer_2158_small.jpg"
-        }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 600) {
-            ...GatsbyImageSharpFluid
+        edges {
+          node {
+            name
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 500) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
           }
         }
       }
@@ -109,20 +159,15 @@ export default function FitnessInfo() {
         publicURL
         name
       }
-      allFile(filter: { relativePath: { regex: "/landing-page/carousel/" } }) {
-        edges {
-          node {
-            name
-            childImageSharp {
-              fluid(quality: 90, maxWidth: 1000) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
-            }
-          }
-        }
-      }
     }
   `);
+
+  console.log(edges);
+
+  const offers = getFitnessOffers(
+    edges.map(x => x.node),
+    { egymLogo }
+  );
 
   return (
     <div className={classes.container} id="studio">
@@ -171,103 +216,41 @@ export default function FitnessInfo() {
           Jetzt Mitglied werden
         </Button>
       </Box>
-      <>
+      <Box mt={8}>
         <Typography variant="h4" align="center" gutterBottom>
           Was wir bieten
         </Typography>
-        <Grid container spacing={4} style={{ marginBottom: 32 }}>
-          <Grid item md={6} xs={12}>
-            <Card>
-              <CardMedia
-                style={{ height: 250 }}
-                component={Img}
-                fluid={egymApp.childImageSharp.fluid}
-                title="egym"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Entdecke die nächste Generation Fitnessgeräte von
-                  <Img
-                    fixed={egym.childImageSharp.fixed}
-                    style={{ marginLeft: 12 }}
-                  />
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Der egym Zirkel in deinem 1a fit ermöglicht es dir, mit
-                  innovativer Software schnell deine Trainingsziele zu
-                  erreichen. Mit Unterstützung von Deinem Trainer kannst Du über
-                  einen Bildschirm Dein individuelles Trainingsprogramm wählen.
-                  Abnehmen, Figurtraining, Verbesserung der allgemeinen Fitness,
-                  Muskelaufbau oder Reha Training – das Training am eGym-Zirkel
-                  lässt keine Wünsche oder Ziele offen.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Mehr erfahren
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <Card
-              style={{
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <CardMedia
-                style={{ height: 250 }}
-                component={Img}
-                fluid={egymApp.childImageSharp.fluid}
-                title="egym"
-              />
-              <CardContent style={{ flex: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Getränkeflatrate - Trinken Sie was Sie wollen
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Egal ob gekühltes Wasser oder aromatisierte Getränke - für nur
-                  4,95€ / Monat können Sie sich ihre 1a fit Flasche so oft
-                  auffüllen, wie Sie wollen.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Mehr erfahren
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <Card>
-              <CardMedia
-                style={{ height: 250 }}
-                component={Img}
-                fluid={globus.childImageSharp.fluid}
-                title="egym"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Galileo Training - effektiv und gesund trainieren
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Mit integrierter Wobbel-Funktion: Das Galileo Tool zum
-                  effektiven Training von Gleichgewicht und Koordination. Mit
-                  integrierter Smart Coaching-Funktion: Passt das Training
-                  automatisch an die Fähigkeiten des Benutzers an.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Mehr erfahren
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+        <Grid container spacing={4}>
+          {offers.map(x => (
+            <Grid key={x.desc} item md={4} sm={6} xs={12}>
+              <Card
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  style={{ height: 250 }}
+                  component={Img}
+                  fluid={x.imgFluid}
+                  title={x.desc}
+                />
+                <CardContent style={{ flex: 1 }}>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    {x.title}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Mehr erfahren
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      </>
+      </Box>
     </div>
   );
 }
