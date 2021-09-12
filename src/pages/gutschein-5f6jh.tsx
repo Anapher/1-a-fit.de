08bfff752178b61a2b0b44ab827891b959af213a
Vincent (Anapher) <anapher@protonmail.com>
2021-09-12 12:11:42 +0200

    completely remove gatsby-image

--- src/pages/gutschein-5f6jh.tsx
+++ src/pages/gutschein-5f6jh.tsx
@@ -1,108 +1,86 @@
-import { Typography, makeStyles, Button, Grid } from "@material-ui/core";
-import { graphql, useStaticQuery } from "gatsby";
-import React from "react";
-import Layout from "../components/Layout";
-import SEO from "../components/seo";
-import { container } from "../style/shared";
-import Img from "gatsby-image";
+import { Button, Grid, makeStyles, Typography } from '@material-ui/core';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+import { container } from '../style/shared';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    ...container,
-    paddingTop: 32,
-    paddingBottom: 32,
-  },
-  voucherImage: {
-    borderRadius: theme.shape.borderRadius,
-    boxShadow: theme.shadows[8],
-    maxWidth: 600,
-  },
-  voucherContainer: {
-    margin: theme.spacing(4, 0),
-  },
+const useStyles = makeStyles((theme) => ({
+   container: {
+      ...container,
+      paddingTop: 32,
+      paddingBottom: 32,
+   },
+   voucherImage: {
+      borderRadius: theme.shape.borderRadius,
+      boxShadow: theme.shadows[8],
+      maxWidth: 600,
+   },
+   voucherContainer: {
+      margin: theme.spacing(4, 0),
+   },
 }));
 
 export default function Gutschein2021() {
-  const classes = useStyles();
+   const classes = useStyles();
 
-  const {
-    voucher1,
-    voucher2,
-    voucherFile1: { publicURL: voucherUrl1 },
-    voucherFile2: { publicURL: voucherUrl2 },
-  } = useStaticQuery(graphql`
-    query {
-      voucher1: file(relativePath: { eq: "voucher/voucher_5f6jh.png" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 600) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
+   const {
+      voucher1,
+      voucher2,
+      voucherFile1: { publicURL: voucherUrl1 },
+      voucherFile2: { publicURL: voucherUrl2 },
+   } = useStaticQuery(graphql`
+      query {
+         voucher1: file(relativePath: { eq: "voucher/voucher_5f6jh.png" }) {
+            childImageSharp {
+               gatsbyImageData(layout: CONSTRAINED, width: 500)
+            }
+         }
+         voucher2: file(relativePath: { eq: "voucher/voucher_k2cgi.png" }) {
+            childImageSharp {
+               gatsbyImageData(layout: CONSTRAINED, width: 500)
+            }
+         }
+         voucherFile1: file(relativePath: { eq: "voucher/voucher_5f6jh.pdf" }) {
+            publicURL
+         }
+         voucherFile2: file(relativePath: { eq: "voucher/voucher_k2cgi.pdf" }) {
+            publicURL
+         }
       }
-      voucher2: file(relativePath: { eq: "voucher/voucher_k2cgi.png" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 600) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-      voucherFile1: file(relativePath: { eq: "voucher/voucher_5f6jh.pdf" }) {
-        publicURL
-      }
-      voucherFile2: file(relativePath: { eq: "voucher/voucher_k2cgi.pdf" }) {
-        publicURL
-      }
-    }
-  `);
+   `);
 
-  return (
-    <Layout>
-      <SEO title="Gutschein" />
-      <div className={classes.container}>
-        <Typography variant="h4">Zwei Gutscheine verfügbar</Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <div className={classes.voucherContainer}>
-              <Img
-                className={classes.voucherImage}
-                fluid={voucher1.childImageSharp.fluid}
-              />
-            </div>
-            <Typography gutterBottom>
-              Gutschein einfach ausdrucken und mitbringen oder diese Website
-              vorzeigen.
-            </Typography>
-            <Button
-              variant="contained"
-              color="secondary"
-              href={voucherUrl1}
-              target="_blank"
-            >
-              Hier PDF herunterladen
-            </Button>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <div className={classes.voucherContainer}>
-              <Img
-                className={classes.voucherImage}
-                fluid={voucher2.childImageSharp.fluid}
-              />
-            </div>
-            <Typography gutterBottom>
-              Gutschein einfach ausdrucken und mitbringen oder diese Website
-              vorzeigen.
-            </Typography>
-            <Button
-              variant="contained"
-              color="secondary"
-              href={voucherUrl2}
-              target="_blank"
-            >
-              Hier PDF herunterladen
-            </Button>
-          </Grid>
-        </Grid>
-      </div>
-    </Layout>
-  );
+   return (
+      <Layout>
+         <SEO title="Gutschein" />
+         <div className={classes.container}>
+            <Typography variant="h4">Zwei Gutscheine verfügbar</Typography>
+            <Grid container spacing={2}>
+               <Grid item xs={12} md={6}>
+                  <div className={classes.voucherContainer}>
+                     <GatsbyImage className={classes.voucherImage} image={getImage(voucher1)} alt="" />
+                  </div>
+                  <Typography gutterBottom>
+                     Gutschein einfach ausdrucken und mitbringen oder diese Website vorzeigen.
+                  </Typography>
+                  <Button variant="contained" color="secondary" href={voucherUrl1} target="_blank">
+                     Hier PDF herunterladen
+                  </Button>
+               </Grid>
+               <Grid item xs={12} md={6}>
+                  <div className={classes.voucherContainer}>
+                     <GatsbyImage className={classes.voucherImage} image={getImage(voucher2)} alt="" />
+                  </div>
+                  <Typography gutterBottom>
+                     Gutschein einfach ausdrucken und mitbringen oder diese Website vorzeigen.
+                  </Typography>
+                  <Button variant="contained" color="secondary" href={voucherUrl2} target="_blank">
+                     Hier PDF herunterladen
+                  </Button>
+               </Grid>
+            </Grid>
+         </div>
+      </Layout>
+   );
 }
--- src/pages/location/vellmar.tsx
+++ src/pages/location/vellmar.tsx
@@ -1,76 +1,75 @@
-import React from "react";
-import Layout from "../../components/Layout";
-import SEO from "../../components/seo";
-import { useStaticQuery, graphql } from "gatsby";
-import Carousel from "../../components/Carousel";
-import { makeStyles, Typography } from "@material-ui/core";
-import { container } from "../../style/shared";
+import React from 'react';
+import Layout from '../../components/Layout';
+import SEO from '../../components/seo';
+import { useStaticQuery, graphql } from 'gatsby';
+import Carousel from '../../components/Carousel';
+import { makeStyles, Typography } from '@material-ui/core';
+import { container } from '../../style/shared';
+import { getImage } from 'gatsby-plugin-image';
 
 const useStyles = makeStyles({
-  container: { ...container, paddingTop: 32, paddingBottom: 32 },
+   container: { ...container, paddingTop: 32, paddingBottom: 32 },
 });
 
 export default function vellmar() {
-  const {
-    site: {
-      siteMetadata: {
-        maps: { vellmar },
+   const {
+      site: {
+         siteMetadata: {
+            maps: { vellmar },
+         },
       },
-    },
-    allFile: { edges },
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          maps {
-            vellmar {
-              src
-              info
+      allFile: { edges },
+   } = useStaticQuery(graphql`
+      {
+         site {
+            siteMetadata {
+               maps {
+                  vellmar {
+                     src
+                     info
+                  }
+               }
             }
-          }
-        }
-      }
-      allFile(filter: { relativePath: { regex: "/carousel/vellmar/" } }) {
-        edges {
-          node {
-            name
-            childImageSharp {
-              fluid(quality: 90, maxWidth: 800) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
+         }
+         allFile(filter: { relativePath: { regex: "/carousel/vellmar/" } }) {
+            edges {
+               node {
+                  name
+                  childImageSharp {
+                     gatsbyImageData(layout: CONSTRAINED)
+                  }
+               }
             }
-          }
-        }
+         }
       }
-    }
-  `);
+   `);
 
-  const classes = useStyles();
+   const classes = useStyles();
 
-  return (
-    <Layout>
-      <SEO title="Vellmar" />
-      <div id="vellmar">
-        <iframe
-          src={vellmar.src}
-          frameBorder={0}
-          allowFullScreen={false}
-          tabIndex={0}
-          style={{ border: 0 }}
-          width="100%"
-          height={400}
-        />
-        <div className={classes.container}>
-          <Typography variant="h6" gutterBottom>
-            Eindrücke von unserem Fitnessstudio in Vellmar
-          </Typography>
-          <Carousel
-            images={edges.map(x => ({
-              fluid: x.node.childImageSharp.fluid,
-            }))}
-          />
-        </div>
-      </div>
-    </Layout>
-  );
+   return (
+      <Layout>
+         <SEO title="Vellmar" />
+         <div id="vellmar">
+            <iframe
+               src={vellmar.src}
+               frameBorder={0}
+               allowFullScreen={false}
+               tabIndex={0}
+               style={{ border: 0 }}
+               width="100%"
+               height={400}
+            />
+            <div className={classes.container}>
+               <Typography variant="h6" gutterBottom>
+                  Eindrücke von unserem Fitnessstudio in Vellmar
+               </Typography>
+               <Carousel
+                  images={edges.map((x) => ({
+                     data: getImage(x.node),
+                  }))}
+               />
+            </div>
+         </div>
+      </Layout>
+   );
 }
